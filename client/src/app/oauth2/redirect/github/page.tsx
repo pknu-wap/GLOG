91047c7617ef8aa5332827d629649a16b41183eb
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-09-18 00:03:41 +0900

    프리렌더링 오류 수정

--- client/src/app/oauth2/redirect/github/page.tsx
+++ client/src/app/oauth2/redirect/github/page.tsx
@@ -1,13 +1,18 @@
-/* app/blog/[slug]/page.tsx */
-'use client';
-
 import { useRouter } from 'next/router';
 import React from 'react';
 
 const Page = () => {
   const router = useRouter();
 
-  console.log(router);
+  try {
+    // 서버 사이드 렌더링 중에 발생할 수 있는 코드
+    // 예를 들어, 데이터 가져오기 등
+    console.log(router);
+  } catch (error) {
+    console.error('프리렌더링 중 오류 발생:', error);
+    // 오류 처리 또는 오류 페이지로 리다이렉션
+    // 예: router.push('/error');
+  }
 
   return <div>깃허브 성공</div>;
 };

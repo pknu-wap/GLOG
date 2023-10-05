e8e18b8019c7ccbda1edfacc397107a55dfd9564
JongKyeong Kim <oo7bangjk@naver.com>
2023-10-05 21:22:31 +0900

    PostPreviewDto 수정

--- server/src/main/java/com/project/Glog/dto/responsee/post/PostPreviewDto.java
+++ server/src/main/java/com/project/Glog/dto/responsee/post/PostPreviewDto.java
@@ -5,26 +5,32 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @AllArgsConstructor
 @Getter @Setter
 public class PostPreviewDto {
+    private String blogUrl;
     private Long postId;
     private String title;
-    private String content;
     private String imageUrl;
     private Integer likesCount;
     private Integer viewsCount;
-    private String hashtags;
+    private Integer repliesCount;
+    private LocalDateTime createdAt;
+    private Boolean isPrivate;
 
     //TODO 어떤 정보 보여줄지 기억이 안남 API 명세 작성 필요.
 
     public static PostPreviewDto of (Post post){
-        return new PostPreviewDto(post.getId(),
+        return new PostPreviewDto(post.getBlogUrl(),
+                post.getId(),
                 post.getTitle(),
-                post.getContent(),
                 post.getImageUrl(),
                 post.getLikesCount(),
                 post.getViewsCount(),
-                post.getHashtags());
+                0,
+                post.getCreatedAt(),
+                post.getIsPrivate());
     }
 }

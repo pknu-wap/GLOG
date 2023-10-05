afad0850d4aa463084b13b5e55e9407eb2cd7bbd
du yeong <doyeong32@gmail.com>
2023-10-05 20:26:56 +0900

    .

--- server/src/main/java/com/project/Glog/domain/Reply.java
+++ server/src/main/java/com/project/Glog/domain/Reply.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "reply")
 public class Reply {
     @Id

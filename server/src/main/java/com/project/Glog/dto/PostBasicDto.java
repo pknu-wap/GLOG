69f44d666e7642d1fd4a3d39c80c4f0386c1dbf0
dy102 <132787602+dy102@users.noreply.github.com>
2023-10-11 11:46:58 +0900

    템플릿 기능 구현 + 템플릿, 임시저장 상위클래스 생성(Dto에 합침)

--- server/src/main/java/com/project/Glog/dto/PostBasicDto.java
+++ server/src/main/java/com/project/Glog/dto/PostBasicDto.java
@@ -19,15 +19,7 @@ public class PostBasicDto {
     private String content;
     private String thumbnail;
     private List<String> hashtags; //TODO 리스트로 주고 받는게 편할듯?
-
-    /*public static PostBasicDto of (Temporary temporary, List<String> hashtags){
-        return new PostBasicDto(
-                temporary.getId(),
-                temporary.getTitle(),
-                temporary.getContent(),
-                temporary.getThumbnail(),
-                hashtags);
-    }*/
+    
     public static <T extends TemplateTemporary> PostBasicDto of(T templateTemporary, List<String> hashtags) {
         return new PostBasicDto(
                 templateTemporary.getId(),

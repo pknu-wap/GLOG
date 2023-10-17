bc5f57ce6784827e696b33e6591669cf493c8a9a
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-10-17 23:41:31 +0900

    #81 Mypage 주/년 달력 구현

--- client/src/app/[blogName]/mypage/page.tsx
+++ client/src/app/[blogName]/mypage/page.tsx
@@ -1,27 +1,50 @@
 'use client';
 
 import CenterContent from '@/components/Layout/CenterContent';
-import { Stack, Typography, useTheme } from '@mui/material';
-import React from 'react';
+import { Stack, ToggleButton, ToggleButtonGroup, Typography, useTheme } from '@mui/material';
+import React, { useState } from 'react';
 import FootPrint from '../../../../public/assets/yellowFootPrint.png';
 import Image from 'next/image';
 import Button from '@/components/Button/Button';
 
 function page() {
   const theme = useTheme();
+  const [yearWeekToggle, setYearWeekToggle] = useState<string | null>('year');
+
+  const handleAlignment = (_: React.MouseEvent<HTMLElement>, newYearWeekToggle: string | null) => {
+    if (newYearWeekToggle !== null) {
+      setYearWeekToggle(newYearWeekToggle);
+    }
+  };
 
   const days = ['월', '화', '수', '목', '금', '토', '일'];
 
   return (
     <>
       <CenterContent maxWidth={'1000px'} sx={{ gap: '64px' }}>
-        <Stack height="250px" spacing={4}>
+        <Stack spacing={4} width="100%">
+          <Stack width="100%" alignItems="flex-end">
+            <ToggleButtonGroup
+              sx={{ height: '32px' }}
+              value={yearWeekToggle}
+              exclusive
+              onChange={handleAlignment}>
+              <ToggleButton color="primary" value="week">
+                1주
+              </ToggleButton>
+              <ToggleButton color="primary" value="year">
+                1년
+              </ToggleButton>
+            </ToggleButtonGroup>
+          </Stack>
           <Stack
             p={10}
+            width="100%"
             borderRadius="8px"
             direction="row"
-            spacing={10}
+            spacing={20}
             border="1px solid #E8E2B4"
+            alignItems="center"
             overflow="scroll"
             bgcolor={theme.palette.mode === 'dark' ? 'transparent' : '#ffffff'}>
             <Stack>
@@ -37,49 +60,73 @@ function page() {
                 <Stack fontSize="14px">(urlurlurlu)</Stack>
               </Stack>
             </Stack>
-            <Stack minWidth="450px" width="100%" height="100%" justifyContent="center" spacing={4}>
-              <Stack
-                borderRadius="8px"
-                width="100%"
-                direction="row"
-                border={`1px solid ${theme.palette.primary.main}`}
-                padding={3}
-                //   bgcolor="red"
-                justifyContent="space-around">
-                {days.map((day, index) => {
-                  return (
-                    <Stack
-                      borderRight={`1px solid ${theme.palette.primary.main}`}
-                      sx={{ ':last-child': { borderRight: 'none' } }}
-                      key={index}
-                      width="14%"
-                      textAlign="center">
-                      {day}
+            <Stack width="100%" height="100%" justifyContent="center" spacing={4}>
+              {yearWeekToggle === 'week' ? (
+                <Stack minWidth="360px" width="100%" height="100%">
+                  <Stack
+                    borderRadius="8px"
+                    width="100%"
+                    direction="row"
+                    border={`1px solid ${theme.palette.primary.main}`}
+                    padding={3}
+                    //   bgcolor="red"
+                    justifyContent="space-around">
+                    {days.map((day, index) => {
+                      return (
+                        <Stack
+                          borderRight={`1px solid ${theme.palette.primary.main}`}
+                          sx={{ ':last-child': { borderRight: 'none' } }}
+                          key={index}
+                          width="14%"
+                          textAlign="center">
+                          {day}
+                        </Stack>
+                      );
+                    })}
+                  </Stack>
+                  <Stack
+                    borderRadius="8px"
+                    width="100%"
+                    direction="row"
+                    //   bgcolor="red"
+                    padding={3}
+                    justifyContent="space-around">
+                    {days.map((_, index) => {
+                      return (
+                        <Stack
+                          borderLeft={`1px solid ${theme.palette.primary.main}`}
+                          sx={{ ':first-child': { borderLeft: 'none' } }}
+                          key={index}
+                          width="14%"
+                          justifyContent="flex-start"
+                          alignItems="center">
+                          <Image src={FootPrint} alt="footPrint" width="30" height="30" />
+                        </Stack>
+                      );
+                    })}
+                  </Stack>
+                </Stack>
+              ) : (
+                <Stack spacing={'1px'} width="100%" gap={1}>
+                  {Array.from({ length: 12 }, (_, i) => (
+                    <Stack direction="row" width="100%" gap={2} key={i} marginRight="12px">
+                      <Stack lineHeight="14px" height="14px">
+                        Jan
+                      </Stack>
+                      <Stack spacing={'5px'} direction="row">
+                        {Array.from({ length: 30 }, (_, index) => (
+                          <Stack
+                            key={index}
+                            width="14px"
+                            height="14px"
+                            border={`1px solid ${theme.palette.primary.main}`}
+                          />
+                        ))}
+                      </Stack>
                     </Stack>
-                  );
-                })}
-              </Stack>
-              <Stack
-                borderRadius="8px"
-                width="100%"
-                direction="row"
-                //   bgcolor="red"
-                padding={3}
-                justifyContent="space-around">
-                {days.map((_, index) => {
-                  return (
-                    <Stack
-                      borderLeft={`1px solid ${theme.palette.primary.main}`}
-                      sx={{ ':first-child': { borderLeft: 'none' } }}
-                      key={index}
-                      width="14%"
-                      justifyContent="flex-start"
-                      alignItems="center">
-                      <Image src={FootPrint} alt="footPrint" width="30" height="30" />
-                    </Stack>
-                  );
-                })}
-              </Stack>
+                  ))}
+                </Stack>
+              )}
             </Stack>
           </Stack>
         </Stack>

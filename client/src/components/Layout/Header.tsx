6893f7246f8ad578e41301f6a2362ce9a4c6ace0
dev-junseo <80705329+dev-junseo@users.noreply.github.com>
2023-10-28 23:17:16 +0900

    pr 전 마지막 수

--- client/src/components/Layout/Header.tsx
+++ client/src/components/Layout/Header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
   const router = useRouter();
   const [userTheme, setUserTheme] = useUserThemeSSR();
 
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState<boolean>(false);
   const backendFriendInfo = [
     {	
       userFriendResponse : {
@@ -121,6 +121,7 @@ export default function Header() {
           <MenuItem onClick={() => handleClose('mypage')}>마이페이지</MenuItem>
           <MenuItem onClick={() => handleClose('friend')}>
             친구
+            
             <Modal open={open} maxWidth="lg" onClose={() => setOpen(false)}>
               <ModalContent>
                 <Stack 
@@ -131,7 +132,7 @@ export default function Header() {
                 //반응형(Mobile L - 425px 부터 미흡)
                 width="50vw"
                 height="100%"
-                maxWidth="500px"
+                maxWidth="550px"
                 padding="10px 20px">
                   <TopStack>
                     <Stack marginBottom="5px">

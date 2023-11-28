50a6fa060b02c3798ad957e897843fcbe105cabb
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-11-29 00:12:43 +0900

    config 변경

--- client/next.config.js
+++ client/next.config.js
@@ -12,47 +12,48 @@ const removeImports = require('next-remove-imports')();
 //   font-src 'self';
 //   frame-src giscus.app
 // `;
-const securityHeaders = [
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
-  {
-    key: 'Content-Security-Policy',
-    value: 'upgrade-insecure-requests',
-    // value: ContentSecurityPolicy.replace(/\n/g, ''),
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-  {
-    key: 'Referrer-Policy',
-    value: 'strict-origin-when-cross-origin',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
-  {
-    key: 'X-Frame-Options',
-    value: 'DENY',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
-  {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=31536000; includeSubDomains',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
-  {
-    key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=()',
-  },
-];
+// const securityHeaders = [
+//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+//   {
+//     key: 'Content-Security-Policy',
+//     value: 'upgrade-insecure-requests',
+//     // value: ContentSecurityPolicy.replace(/\n/g, ''),
+//   },
+//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+//   {
+//     key: 'Referrer-Policy',
+//     value: 'strict-origin-when-cross-origin',
+//   },
+//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+//   {
+//     key: 'X-Frame-Options',
+//     value: 'DENY',
+//   },
+//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+//   {
+//     key: 'X-Content-Type-Options',
+//     value: 'nosniff',
+//   },
+//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+//   {
+//     key: 'X-DNS-Prefetch-Control',
+//     value: 'on',
+//   },
+//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+//   {
+//     key: 'Strict-Transport-Security',
+//     value: 'max-age=31536000; includeSubDomains',
+//   },
+//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+//   {
+//     key: 'Permissions-Policy',
+//     value: 'camera=(), microphone=(), geolocation=()',
+//   },
+// ];
 
 const nextConfig = {
   // ...options
+  output: 'export',
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
@@ -69,26 +70,27 @@ const nextConfig = {
       },
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/collect',
-        permanent: true,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       destination: '/collect',
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
 
-  async headers() {
-    return [
-      {
-        source: '/glogglogglog-env.eba-fuksumx7.ap-northeast-2.elasticbeanstalk.com',
-        headers: securityHeaders,
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/glogglogglog-env.eba-fuksumx7.ap-northeast-2.elasticbeanstalk.com',
+  //       headers: securityHeaders,
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = removeImports({
   ...nextConfig,
+  output: 'export',
 });

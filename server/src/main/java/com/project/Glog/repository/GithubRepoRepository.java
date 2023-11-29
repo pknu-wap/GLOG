014982bc828d3fbb39f0a3aa2fe867503dc4136c
JongKyeong Kim <oo7bangjk@naver.com>
2023-11-30 03:51:27 +0900

    깃허브 수정

--- server/src/main/java/com/project/Glog/repository/GithubRepoRepository.java
+++ server/src/main/java/com/project/Glog/repository/GithubRepoRepository.java
@@ -14,8 +14,8 @@ public interface GithubRepoRepository extends JpaRepository<GithubRepository, Lo
     @Query("SELECT gr FROM GithubRepository gr WHERE gr.user.id = :userId AND gr.repoName= :reponame AND gr.isCategoryRegi=false")
     Optional <GithubRepository> findByRepoName(@Param("userId") Long userid , @Param("reponame") String reponame);
 
-    @Query("SELECT gr FROM GithubRepository gr WHERE gr.user.id = :userId AND gr.repoName=:repo")
-    Optional <GithubRepository> findRepoByUserId(@Param("userId") Long userid, @Param("repo") String repo);
+    @Query("SELECT gr FROM GithubRepository gr WHERE gr.user.id = :userId AND gr.repoName=:repo AND gr.category.id=:categoryId")
+    Optional <GithubRepository> findRepoByUserId(@Param("userId") Long userid, @Param("repo") String repo,@Param("categoryId") Long categoryId);
 
     @Query("SELECT gr.repoName FROM GithubRepository gr WHERE gr.user.id = :userId AND gr.isCategoryRegi=false")
     List<String> findRepoNameByUserId(@Param("userId") Long userid);

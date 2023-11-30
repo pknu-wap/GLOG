2917f800975a82a5610cc954a71b8bdd0565af19
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-11-30 19:04:10 +0900

    좋아요 이름 변경

--- client/src/app/collect/page.tsx
+++ client/src/app/collect/page.tsx
@@ -22,7 +22,7 @@ function Collect() {
       {!isSearch ? (
         <Stack padding={'10px 40px'} spacing={4}>
           <Typography color="oppositeColor.main" fontSize="24px">
-            좋아요 한 게시글
+            추천수가 많은 게시글
           </Typography>
           <CollectArray kind="likes" />
           <Typography color="oppositeColor.main" fontSize="24px">

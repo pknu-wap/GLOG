593c9d5750ec9278d71db700b806e33f343bbbe0
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-12-14 18:28:52 +0900

    #161 data 제거

--- client/src/app/[blogName]/home/[categoryId]/[postId]/page.tsx
+++ client/src/app/[blogName]/home/[categoryId]/[postId]/page.tsx
@@ -1,29 +1,39 @@
 import PostData from '@/components/Post/PostData';
-import { SERVER_URL } from '@/constant/common';
+// import { SERVER_URL } from '@/constant/common';
 import { Metadata } from 'next';
 import React from 'react';
 
-const getData = async (id: number) => {
-  const res = await fetch(`${SERVER_URL}/post?postId=${id}`);
-  return res.json();
-};
+// const getData = async (id: number) => {
+//   const res = await fetch(`${SERVER_URL}/post?postId=${id}`);
+//   return res.json();
+// };
 
-export const generateMetadata = async ({
-  params,
-}: {
-  params: { id: string };
-}): Promise<Metadata> => {
-  const data = await getData(Number(params.id));
-
+export const generateMetadata = async (): Promise<Metadata> => {
   return {
-    title: data.title,
-    description: data.content,
+    title: '타이틀입니다.',
+    description: 'ㅁㄴㅇㄹ',
     openGraph: {
-      title: data.title,
-      description: data.content,
+      title: 'openGraph 타이틀이빈다.',
+      description: 'ㅁㄴㅇㄹ',
     },
   };
 };
+// export const generateMetadata = async ({
+//   params,
+// }: {
+//   params: { id: string };
+// }): Promise<Metadata> => {
+//   const data = await getData(Number(params.id));
+
+//   return {
+//     title: '타이틀입니다.',
+//     description: 'ㅁㄴㅇㄹ',
+//     openGraph: {
+//       title: 'openGraph 타이틀이빈다.',
+//       description: 'ㅁㄴㅇㄹ',
+//     },
+//   };
+// };
 
 function page({ params }: { params: { blogName: string; categoryId: string; postId: string } }) {
   return (

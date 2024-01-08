8b20bdc17c6a8fcb3b0dc24de340cc35cd63e197
JongKyeong Kim <oo7bangjk@naver.com>
2024-01-08 11:57:10 +0900

    배배

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-REPOSITORY=/home/ubuntu/GLOGG
+REPOSITORY=/home/ubuntu/GLOG/server
 cd $REPOSITORY
 
-APP_NAME=GLOGG
+APP_NAME=GLOG
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
@@ -18,9 +18,8 @@ else
   sleep 5
 fi
 
-cd /home/ubuntu/GLOGG/server/build/libs
 echo "> Deploy - $JAR_PATH "
-nohup java -jar Glog-0.0.1-SNAPSHOT.jar &
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
 #nohup java -jar $JAR_PATH &
 #nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
 #nohup java -jar $JAR_PATH --spring.profiles.active=prod > /dev/null 2> /dev/null < /dev/null &

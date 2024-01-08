9c1646a3d54603c7f7ee64457025e81a7c503b13
JongKyeong Kim <oo7bangjk@naver.com>
2024-01-08 11:18:57 +0900

    배포

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,7 +19,6 @@ else
 fi
 
 echo "> Deploy - $JAR_PATH "
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
-
-## Option
+nohup java -jar $JAR_PATH &
+#nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
 #nohup java -jar $JAR_PATH --spring.profiles.active=prod > /dev/null 2> /dev/null < /dev/null &

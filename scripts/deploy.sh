b54e87c88b65b2647a2693252cb78279de4737d5
JongKyeong <111286262+JONG-KYEONG@users.noreply.github.com>
2024-01-08 12:15:32 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,4 +19,5 @@ else
 fi
 
 echo "> Deploy - $JAR_PATH "
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+#nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar $JAR_PATH &
